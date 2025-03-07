67de7ffe30938dfff25cd8d78b8226d8a599dc43
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-03-08 00:34:04 +0200

    create show werd screen

---
+++ lib/screens/show_werd_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShowWerdScreen extends StatelessWidget {
+  const ShowWerdScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
